13f7429df4634861b9676d98505e9a359eb2651c
ilopez89 <77321343+ilopez89@users.noreply.github.com>
2021-01-30 19:11:48 +0100

    Segundo cambio en la rama.

---
+++ actividad1/src/actividad1/Cambio2.java
@@ -0,0 +1,12 @@
+package actividad1;
+
+public class Cambio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola clase de Entornos");
+		
+	}
+
+}
